e727918c0bab61b3fe6f6949bf9831bc77fde431
Jonatan P <35601500+JonatanPerezLopez@users.noreply.github.com>
2020-05-30 18:57:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/backgroundservice/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.backgroundservice;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView text = findViewById(R.id.text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (text.getText().toString().equals("Started")) {
+                    text.setText("Stopped");
+                    stopService(new Intent(MainActivity.this,service.class));
+                } else {
+                    text.setText("Started");
+                    startService(new Intent(MainActivity.this,service.class));
+                }
+            }
+        });
+    }
+}
